aba166b64ac5c849286e27c444e1ffa6ffcb3ee4
alyuza <alyuza.sat@gmail.com>
2024-02-18 02:14:53 +0700

    add memes

--- src/components/MainPage/SoonPage/index.jsx
+++ src/components/MainPage/SoonPage/index.jsx
@@ -1,16 +1,15 @@
 import {
-  Typography,
   Box
 } from "@mui/material";
 import comingsoonimage from '../../../assets/comingSoon.png'
+import kepala from '../../../assets/kepala.png'
 import styles from './soon.module.scss'
 
 export default function index() {
   return (
     <Box className={styles.soonPage}>
-      <Typography>
         <img className={styles.soonImage} src={comingsoonimage} alt="Alyuza Photo" />
-      </Typography>
+        <img className={styles.kepalaImage} src={kepala} alt="Alyuza Photo" />
     </Box>
   )
 }
