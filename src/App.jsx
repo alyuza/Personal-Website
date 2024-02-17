3edd4c3f3867d373e414f95b571fc725e981be0e
alyuza <alyuza.sat@gmail.com>
2024-02-18 00:04:09 +0700

    add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { MainPage } from './components'
-
+import { NavBar, Footer } from './components'
+import { ThemeProvider } from '@emotion/react'; // Import ThemeProvider from Emotion
+import theme from '../theme'; // Import your custom theme
+import { HomeSection, SoonPage } from './components/MainPage';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}> {/* Wrap your application with ThemeProvider */}
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<HomeSection />} />
+            <Route path='/about' element={<SoonPage />} />
+            <Route path='/technologies' element={<SoonPage />} />
+            <Route path='/projects' element={<SoonPage />} />
+            <Route path='/contact' element={<SoonPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </>
   )
 }
 
-export default App
+export default App;
