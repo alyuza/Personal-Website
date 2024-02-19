fd02f457af0ed949ff348088f8ff627421118c7b
alyuza <alyuza.sat@gmail.com>
2024-02-19 22:51:01 +0700

    styling : fix padding for desktop and mobile

--- src/components/MainPage/ComingSoon/index.jsx
+++ src/components/MainPage/ComingSoon/index.jsx
@@ -2,14 +2,12 @@ import {
   Box
 } from "@mui/material";
 import comingsoonimage from '../../../assets/comingSoon.png'
-import kepala from '../../../assets/kepala.png'
 import styles from './soon.module.scss'
 
 export default function index() {
   return (
     <Box className={styles.soonPage}>
         <img className={styles.soonImage} src={comingsoonimage} alt="Alyuza Photo" />
-        {/* <img className={styles.kepalaImage} src={kepala} alt="Alyuza Photo" /> */}
     </Box>
   )
 }
